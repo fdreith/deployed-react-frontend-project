afc339210313718245fba5b5510c698016273fab
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-24 20:00:06 -0700

    pass renderTaskInfo to Task component

--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import Task from './Task.js'
-
+import React from "react";
+import Task from "./Task.js";
 
 const MyTasks = (props) => {
-
   return (
-    < div className="myTasks" >
+    <div className="myTasks">
       <h6>{props.currentUser.attributes.name}'s Tasks:</h6>
       <ul>
-        {props.myTasks.map(task => {
+        {props.myTasks.map((task) => {
           return (
             <div key={task.id}>
-              <Task task={task} history={props.history} />
-            </div>)
+              <Task
+                task={task}
+                history={props.history}
+                renderTaskInfo={props.renderTaskInfo}
+              />
+            </div>
+          );
         })}
       </ul>
-    </div >
-  )
+    </div>
+  );
+};
 
-}
-
-export default MyTasks
+export default MyTasks;
