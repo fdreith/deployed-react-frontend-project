f3b2b058addca264aa32776c51be604646b3f31f
Francesca Dreith <francesca.dreith@gmail.com>
2020-09-04 10:03:25 -0700

    add splitColumn to renderTaskForm

--- src/components/TasksContainer.js
+++ src/components/TasksContainer.js
@@ -45,6 +45,7 @@ class TasksContainer extends Component {
   renderTaskForm = () => {
     this.setState({
       renderTaskForm: !this.state.renderTaskForm,
+      splitColumn: !this.state.splitColumn,
     });
   };
 
